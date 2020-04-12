188f7033a72844ebaa61b1da6deee0fac437c88b
simplem4n <quan.tran@streamy.live>
2020-04-12 23:12:20 +0700

    add swap cancel behaviour

--- lib/ex_okex/api_swap_client_behaviour.ex
+++ lib/ex_okex/api_swap_client_behaviour.ex
@@ -8,6 +8,7 @@ defmodule ExOkex.ApiSwapClientBehaviour do
   @callback create_order(params, config) :: response
   @callback create_bulk_orders(params, config) :: response
   @callback cancel_orders(String.t(), list, map, config) :: response
+  @callback cancel_order(any, any, config) :: response
   @callback get_position(String.t(), config) :: response
   @callback get_leverage(String.t(), config) :: response
 end
