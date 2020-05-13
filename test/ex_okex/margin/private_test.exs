dcfa5d8518a3d68a65fdd332ad020d7aef003a4e
Hieu Lam <hieu@acuity.sg>
2020-05-13 11:56:00 +0700

    Add api to create single/multiple orders

---
+++ test/ex_okex/margin/private_test.exs
@@ -0,0 +1,14 @@
+defmodule ExOkex.Futures.PrivateTest do
+    use ExUnit.Case, async: true
+    import TestHelper
+    alias ExOkex.Margin.Private, as: Api
+
+    describe "test place single order" do
+    end
+
+    describe "test place multiple orders" do
+    end
+
+    
+
+end
