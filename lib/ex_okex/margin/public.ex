852d5ae0b35cd8ee0cb218eac61ddf7d939094d6
simplem4n <quan.tran@streamy.live>
2020-10-15 14:06:30 +0700

    add exchange info endpoint

--- lib/ex_okex/margin/public.ex
+++ lib/ex_okex/margin/public.ex
@@ -7,6 +7,10 @@ defmodule ExOkex.Margin.Public do
     get("/api/spot/v3/instruments/#{instrument}/ticker", %{}, nil)
   end
 
+  def get_exchange_info() do
+    get("/api/spot/v3/instruments", %{}, nil)
+  end
+
   @spec get_mark_price(String.t()) :: {:ok, map}
   def get_mark_price(instrument) do
     get("/api/margin/v3/instruments/#{instrument}/mark_price", %{}, nil)
